c0b02a4886feadbfb1211835cbccc752335cd284
marketcoach <73725675+marketcoach@users.noreply.github.com>
2020-11-01 19:03:01 -0500

    Add files via upload

---
+++ HWK1/CHAPTER2/EXCERCISE2/welcomeuser.py
@@ -0,0 +1,2 @@
+UserInput=input("What is your name?")
+print("Welcome User Take A Seat "+UserInput)
---
+++ HWK1/CHAPTER2/EXCERCISE3/pay.py
@@ -0,0 +1,3 @@
+hours = float(input("How many HOURS did you work this week? "))
+rate = float(input("How much are you PAID per hour? "))
+print("PAY IS: $" + str(hours * rate))
